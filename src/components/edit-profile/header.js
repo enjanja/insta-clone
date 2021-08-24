901610ec60cf1f58f205214fc70ad853f4d8ffc4
enjanja <57662248+enjanja@users.noreply.github.com>
2021-08-24 16:35:12 +0200

    update - dodatak

---
+++ src/components/edit-profile/header.js
@@ -0,0 +1,52 @@
+/* eslint-disable prettier/prettier */
+// import { useContext } from 'react'
+import {  useHistory } from 'react-router-dom'
+// import FirebaseContext from '../../context/firebase'
+// import UserContext from '../../context/user'
+// import useUser from '../hooks/use-user'
+import * as ROUTES from '../../constants/routes'
+
+export default function Header() {
+//   const { user: loggedInUser } = useContext(UserContext)
+  // const { user } = useUser(loggedInUser?.uid)
+//   const { firebase } = useContext(FirebaseContext)
+  const history = useHistory()
+
+  return (
+    <header className="h-16 bg-white border-b border-gray-primary home-header">
+      <div className="container mx-auto max-w-screen-lg h-full">
+        <div className="flex justify-between h-full">
+          <div className="text-gray-700 text-center flex items-center align-items cursor-pointer ml-3">
+                <button
+                  type="button"
+                  title="back"
+                  onClick={() => {
+                    history.goBack()
+                    history.push(ROUTES.PROFILE)
+                    // history.goBack()
+                  }}
+                  onKeyDown={(event) => {
+                    if (event.key === 'Enter') {
+                      history.goBack()
+                    }
+                  }}
+                >
+                  <svg 
+                    className="w-8 text-black-light cursor-pointer"
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" 
+                    stroke="currentColor">
+                    <path 
+                        strokeLinecap="round"
+                        strokeLinejoin="round" 
+                        strokeWidth="2" 
+                        d="M11 17l-5-5m0 0l5-5m-5 5h12" 
+                    />
+                    </svg>
+                </button>
+          </div>
+        </div>      
+      </div>
+    </header>
+  )
+}
--- src/pages/update-profile.js
+++ src/pages/update-profile.js
@@ -1,146 +1,142 @@
 /* eslint-disable prettier/prettier */
-// /* eslint-disable prettier/prettier */
-// import { useState, useContext, useEffect } from 'react'
-// import {  useHistory } from 'react-router-dom'
-// // import PropTypes from 'prop-types'
-// import FirebaseContext from '../context/firebase'
-// import * as ROUTES from '../constants/routes'
-// import { doesUsernameExist } from '../services/firebase'
-// import UserContext from '../context/user'
-// import Header from '../components/header'
-// import Footer from '../components/footer'
-// // import useUser from '../hooks/use-user'
-// import LoggedInUserContext from '../context/logged-in-user'
-// // import {
-// //   updateLoggedInUserFollowing,
-// //   updateFollowedUserFollowers,
-// //   getUserByUserId
-// // } from '../../services/firebase'
+/* eslint-disable prettier/prettier */
+import { useState, useContext, useEffect } from 'react'
+import {  useHistory } from 'react-router-dom'
+// import PropTypes from 'prop-types'
+import FirebaseContext from '../context/firebase'
+import * as ROUTES from '../constants/routes'
+import useUser from '../hooks/use-user'
+import {
+    // getUserByUserId,
+    doesUsernameExist
+  } from '../services/firebase'
+import UserContext from '../context/user'
+import Header from '../components/edit-profile/header'
 
-// export default function UpdateProfile() {
-//   const history = useHistory()
-//   const { firebase } = useContext(FirebaseContext)
+export default function UpdateProfile() {
+  const history = useHistory()
+  const { firebase } = useContext(FirebaseContext)
 
-//   const { user: loggedInUser } = useContext(UserContext)
-//   // const { user: { docId = '', fullName, username, userId, emailAddress, password } = {} } = useContext(LoggedInUserContext)
+  const { user: loggedInUser } = useContext(UserContext)
+  const { user } = useUser(loggedInUser?.uid)
 
-//   const [usernameUpdate, setUsername] = useState('')
-//   const [fullNameUpdate, setFullName] = useState('')
-//   const [emailAddressUpdate, setEmailAddress] = useState('')
-//   const [passwordUpdate, setPassword] = useState('')
+  const [usernameField, setUsernameField] = useState('')
+  const [fullNameField, setFullNameField] = useState('')
+  const [emailAddressField, setEmailAddressField] = useState('')
 
-//   const [error, setError] = useState('')
-//   const isInvalid = emailAddressUpdate === '' && usernameUpdate === '' && fullNameUpdate === '' && passwordUpdate === ''
+  const [error, setError] = useState('')
+  const isInvalid = usernameField === '' && fullNameField === '' && emailAddressField === '' 
 
-//   const handleUpdate = async (event) => {
-//     event.preventDefault()
+  
+  const handleUpdate = async (event) => {
+    event.preventDefault()
 
-//     const usernameExists = await doesUsernameExist(usernameUpdate)
+    const usernameExists = await doesUsernameExist(usernameField)
+    
+    if (!usernameExists) {
+      try {
 
-//     if (!usernameExists) {
-//       try {
+          let usernameSub
+          let fulldNameSub
+          let emailAddressSub
 
-//         if(username === ''){
-//           setUsername(usernameUpdate)
-//         }
-//         if(emailAddress === ''){
-//           setUsername(emailAddressUpdate)
-//         }
-//         if(fullName === ''){
-//           setUsername(fullNameUpdate)
-//         }
-//         if(password === ''){
-//           setUsername(passwordUpdate)
-//         }
+        if(usernameField.length === 0){
+            usernameSub = user.username
+        }else {
+            usernameSub = usernameField
+        }
+        if(fullNameField.length === 0){
+            fulldNameSub = user.fullName
+        }else {
+            fulldNameSub = fullNameField
+        }
+        if(emailAddressField.length === 0){
+            emailAddressSub = user.emailAddress
+        }else {
+            emailAddressSub = emailAddressField
+        }
 
-//         console.log({usernameUpdate, emailAddressUpdate, fullNameUpdate, passwordUpdate})
+        await firebase
+          .firestore()
+          .collection('users')
+          .doc(user.docId)
+          .update({
+            username: usernameSub.toLowerCase(),
+            fullName: fulldNameSub,
+            emailAddress: emailAddressSub.toLowerCase(),
+          })
 
-//         history.push(ROUTES.PROFILE)
-//       } catch (error) {
-//         setUsername(username)
-//         setEmailAddress(emailAddress)
-//         setFullName(fullName)
-//         setPassword(password)
-//         setError(error.message)
-//       }
-//     } else {
-//       setUsername('')
-//       setError('That username is already taken, please try another.')
-//     }
-//   }
+        history.push(ROUTES.UPDATE_PROFILE)
+      } catch (error) {
+        setUsernameField(user.username)
+        setFullNameField(user.fullName)
+        setEmailAddressField(user.emailAddress)
+        setError(error.message)
+      }
+    } else {
+      setError('That username is already taken, please try another.')
+    }
+  }
 
-//   useEffect(() => {
-//     document.title = 'Update profile - Instagram'
-//   }, [])
+  useEffect(() => {
+    document.title = 'Update profile - Instagram'
+  }, [])
 
-//   return (
-//     <div className="bg-gray-background">
-//      <Header />
-//      {/* { user ? ( */}(
-//         <>
-//         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+  return (
+    <div className="bg-gray-background">
+     <Header />
+     { user ? (
+        <>
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
 
-//             <div className="flex flex-col pl-5 pr-5">
-//                 <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
-//                 <p className="pb-5">Edit user info</p>
-//                 {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
-//                 <form onSubmit={handleUpdate} method="PUT">
+            <div className="flex flex-col pl-5 pr-5">
+                <div className="flex flex-col items-center bg-white p-4 border border-gray-primary rounded mb-14">
+                <p className="pb-5">Edit user info</p>
+                {error && <p className="mb-4 text-xs text-red-primary container flex mx-auto max-w-screen-md">{error}</p>}
+                <form onSubmit={handleUpdate} method="POST">
 
-//                     <input
-//                       aria-label="Enter your username"
-//                       type="text"
-//                       placeholder={username}
-//                       className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-//                       onChange={({ target }) => setUsername(target.value)}
-//                       value={username}
-//                     />
-//                     <input
-//                       aria-label="Enter your full name"
-//                       type="text"
-//                       placeholder={fullName}
-//                       className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-//                       onChange={({ target }) => setFullName(target.value)}
-//                       value={fullName}
-//                     />
-//                     <input
-//                       aria-label="Enter your email address"
-//                       type="text"
-//                       placeholder={emailAddress}
-//                       className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-//                       onChange={({ target }) => setEmailAddress(target.value)}
-//                       value={emailAddress}
-//                     />
-//                     <input
-//                     aria-label="Enter your password"
-//                     type="password"
-//                     placeholder={password}
-//                     className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-//                     onChange={({ target }) => setPassword(target.value)}
-//                     value={password}
-//                     />
-//                     <button
-//                     disabled={isInvalid}
-//                     type="submit"
-//                     className={`bg-blue-medium text-white w-full rounded h-8 font-bold
-//                     ${isInvalid && 'opacity-50'}`}
-//                     >
-//                     Update
-//                     </button>
-//                     </form>
-//                 </div>
-//             </div>
-//         </div>
-//         </>
-//           ) : (
-//           <>
-//           <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-//               <p className="flex flex-col pl-5 pr-5">Loading...</p>
-//           </div>
-//           </>
-//           )
+                    <input
+                      aria-label="Enter your username"
+                      type="text"
+                      placeholder={user.username}
+                      className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                      onChange={({ target }) => setUsernameField(target.value)}
+                      value={usernameField}
+                    />
+                    <input
+                      aria-label="Enter your full name"
+                      type="text"
+                      placeholder={user.fullName}
+                      className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                      onChange={({ target }) => setFullNameField(target.value)}
+                      value={fullNameField}
+                    />
+                    <input
+                      aria-label="Enter your email address"
+                      type="text"
+                      placeholder={user.emailAddress}
+                      className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                      onChange={({ target }) => setEmailAddressField(target.value)}
+                      value={emailAddressField}
+                    />
+                    <button
+                    disabled={isInvalid}
+                    type="submit"
+                    className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+                    ${isInvalid && 'opacity-50'}`}
+                    >
+                    Update
+                    </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </>
+        ) : (<></>)     
+    }
 
-//     <Footer />
-//     </div>
+    </div>
 
-//   )
-// }
+  )
+}
+
